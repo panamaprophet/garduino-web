b50a18ffed225b1946189edb6f0d7c485e9e0aff
sergei klykov <me@panamaprophet.com>
2025-04-16 21:49:33 +0200

    wip

--- src/shared/auth/ui/LoginForm/index.tsx
+++ src/shared/auth/ui/LoginForm/index.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Input } from '@/shared/ui/Input';
 import { Button } from '@/shared/ui/Button';
 import { Loader } from '@/shared/ui/Loader';
+// import { Leaf } from '@/shared/ui/Icon';
 
 
 export const LoginForm = ({ onSubmit }: { onSubmit: (credentials: { username: string, password: string }) => Promise<void> }) => {
@@ -10,17 +11,22 @@ export const LoginForm = ({ onSubmit }: { onSubmit: (credentials: { username: st
 
     return (
         <form className="flex flex-col gap-4 p-4 max-w-md mx-auto justify-center h-full">
+            {/* <div className="self-center m-8 flex flex-col gap-2 font-medium items-center">
+                <Leaf className="w-5 h-5 text-emerald-500" /> 
+                <span className="text-slate-600">Garduino</span>
+            </div> */}
+
             <Input
                 value={state.username}
                 onChange={username => setState({ ...state, username })}
-                placeholder="e-mail"
+                placeholder="Email"
                 autoComplete="email"
             />
 
             <Input
                 value={state.password}
                 onChange={password => setState({ ...state, password })}
-                placeholder="password"
+                placeholder="Password"
                 type="password"
                 autoComplete="current-password"
             />
