485a6295c8b89fcb8ed7c5e75ba4cf886daa0719
sergei klykov <pp@rawsound.ru>
2025-04-26 02:21:44 +0200

    feat: schedule using local time (#19)

--- src/entities/configuration/index.tsx
+++ src/entities/configuration/index.tsx
@@ -1,3 +1,3 @@
 export type { Configuration as ControllerConfiguration } from './model/configuration';
 export { queries } from './api/queries';
-export { getConfiguration, updateConfiguration, createConfiguration, getControllerIds } from './api';
+export { getConfiguration, updateConfiguration, createConfiguration, getControllerIds } from './api/api';
--- src/features/controller-historical-data/index.ts
+++ src/features/controller-historical-data/index.ts
@@ -1,2 +1,3 @@
 export { useControllerHistoricalData } from './lib/useControllerHistoricalData';
 export { queries } from './api/queries';
+export { ControllerHistoricalData } from './ui/ControllerHistoricalData';
